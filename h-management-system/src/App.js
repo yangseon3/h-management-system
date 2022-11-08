a9aee1276ba47a55b36072af5430eacecfb82090
hexaspace <hexa6do@naver.com>
2022-11-08 16:38:50 +0900

    init

---
+++ h-management-system/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Sample from './component/sample/SamplePresenter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div>
+          <Sample/>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

e578d63b28435bd0af3bcfe7a1c7bcd42bc3dbe0
onnuri <mathilda0922@gmail.com>
2022-11-15 09:23:21 +0900

    Add: main 페이지 레이아웃

--- h-management-system/src/Router.js
+++ h-management-system/src/Router.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from 'pages/Login/Login';
 import Main from 'pages/Main/Main';
 import Detail from 'pages/Detail/Detail';
+import MainLayout from 'pages/MainLayout/MainLayout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/main" element={<Main />} />
+          <Route path="/detail" element={<Detail />} />
+        </Route>
         <Route path="/" element={<Login />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ h-management-system/src/pages/Main/components/RobotLocation/RobotLocation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './RobotLocation.scss';
+
+const RobotLocation = () => {
+  return (
+    <div className="robotLocatioin">
+      <div>모든 매장모아보기</div>
+      <div>전국 매장위치</div>
+    </div>
+  );
+};
+
+export default RobotLocation;

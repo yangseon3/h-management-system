aa2eaa8bf4cb4044c1ba876eebb45a7c4df3f6df
양동선 <leo@LEO-MackBook.local>
2022-11-16 10:42:54 +0900

    내용수정

--- h-management-system/src/pages/Main/components/MainRobotLocation/MainRobotLocation.js
+++ h-management-system/src/pages/Main/components/MainRobotLocation/MainRobotLocation.js
@@ -3,7 +3,6 @@ import RobotKakaoData from '../RobotLocation/RobotKakaoData/RobotKakaoData';
 import RobotKakaoMap from '../RobotLocation/RobotKakaoMap/RobotKakaoMap';
 import './MainRobotLocation.scss';
 
-
 const MainRobotLocation = () => {
   return (
     <div className="mainRobotLocatioin">

5a3915c43e3e2febe1dcb8c05738062f4ffff35c
zinukk <slsxpseh54@naver.com>
2022-11-21 18:09:30 +0900

    Login Fnc 로직 구현 완료

--- h-management-system/src/pages/Login/Login.js
+++ h-management-system/src/pages/Login/Login.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import { LoginController } from './LoginController';
 import './Login.scss';
 
 const Login = () => {
+  const [validation, setValidation] = useState('');
+
+  const [userInfo, setUserInfo] = useState({
+    userId: '',
+    userPassword: '',
+  });
+
+  const navigate = useNavigate();
+
+  const { userId, userPassword } = userInfo;
+
+  const isValid = userId && userPassword;
+
+  const userInfoHandler = e => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleClickUserInfo = () => {
+    if (!isValid) return setValidation('아이디와 비밀번호 모두 입력해주세요');
+    postUserInfo.mutate(userInfo);
+  };
+
+  const postUserInfo = useMutation(LoginController, {
+    onSuccess: data => {
+      let [err, result] = data;
+      if (!err) {
+        if (result.valid === true) {
+          navigate('/main');
+        } else {
+          setValidation(result);
+        }
+      } else {
+        console.log(result);
+      }
+    },
+  });
   return (
     <div className="login">
       <div className="loginContainer">
         <div className="loginHeader">
           <div className="loginHeaderMark">ServingHelper</div>
           <div className="loginHeaderName">H서빙</div>
-          <input className="loginIdInput" placeholder="아이디"></input>
-          <input className="loginPwInput" placeholder="비밀번호"></input>
+          <input
+            className="loginIdInput"
+            name="userId"
+            placeholder="아이디"
+            value={userId}
+            onChange={userInfoHandler}
+          />
+          <input
+            className="loginPwInput"
+            type="password"
+            name="userPassword"
+            placeholder="비밀번호"
+            value={userPassword}
+            onChange={userInfoHandler}
+          />
+          {validation && <p className="loginValidation">{validation}</p>}
         </div>
         <div>
-          <div className="buttonValid">아이디 또는 비밀번호가 틀렸습니다.</div>
-          <button className="loginButton">로그인</button>
+          <button className="loginButton" onClick={handleClickUserInfo}>
+            로그인
+          </button>
         </div>
         <div className="loginUserinfoFind">
           <a className="loginIdFind">아이디찾기</a>

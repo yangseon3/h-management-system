30323781b7253df31a51f485b3398849adca3294
zinukk <slsxpseh54@naver.com>
2022-12-12 17:40:58 +0900

    Modify : 기능 추가 및 수정

--- h-management-system/src/component/Nav/Nav.js
+++ h-management-system/src/component/Nav/Nav.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 import './Nav.scss';
 
 const Nav = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const page = location.pathname.split('/')[1];
+  const [tabname, setTabname] = useState('HOME');
 
-  console.log(page);
+  const navigate = useNavigate();
+
+  const changeTabNameHandler = name => {
+    setTabname(name);
+  };
 
   return (
     <div className="mainTab">
@@ -17,49 +19,86 @@ const Nav = () => {
           <FaUserCircle className="profileIcon" />
         </div>
         <ul className="navWrap">
-          {SIDE_TAB.map((tab, idx) => (
-            <li
-              key={idx}
-              onClick={() => navigate(tab.path)}
-              style={{
-                backgroundColor:
-                  page === tab.path.split('/')[1] ? '#fff' : 'transparent',
-                color: page === tab.path.split('/')[1] ? '#000' : '#fff',
-              }}
-            >
-              <div className="imgBox">
-                <img
-                  src={
-                    tab.icon +
-                    (page === tab.path.split('/')[1] ? 'bk' : 'wh') +
-                    '.png'
-                  }
-                  alt={tab.tabname}
-                />
-              </div>
-              <p className="menuName">{tab.tabname}</p>
-            </li>
-          ))}
+          {SIDE_TAB.map(({ id, name, icon, activeIcon, path }) =>
+            tabname === name ? (
+              <li
+                className="activeTab"
+                key={id}
+                onClick={() => {
+                  changeTabNameHandler(name);
+                  navigate(path);
+                }}
+              >
+                <div className="imgBox">
+                  <img src={activeIcon} alt="탭아이콘" />
+                </div>
+                <p className="menuName">{name}</p>
+              </li>
+            ) : (
+              <li
+                className="tab"
+                key={id}
+                onClick={() => {
+                  changeTabNameHandler(name);
+                  navigate(path);
+                }}
+              >
+                <div className="imgBox">
+                  <img src={icon} alt="탭아이콘" />
+                </div>
+                <p className="menuName">{name}</p>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </div>
   );
 };
 
-export default Nav;
 const SIDE_TAB = [
-  { tabname: 'HOME', icon: '/images/icons/home_menu_', path: '/main' },
   {
-    tabname: '매장정보',
-    icon: '/images/icons/store_menu_',
+    id: 1,
+    name: 'HOME',
+    icon: '/images/icons/home_menu_wh.png',
+    activeIcon: '/images/icons/home_menu_bk.png',
+    path: '/main',
+  },
+  {
+    id: 2,
+    name: '매장정보',
+    icon: '/images/icons/store_menu_wh.png',
+    activeIcon: '/images/icons/store_menu_bk.png',
     path: '/store/all',
   },
-  { tabname: '로봇', icon: '/images/icons/robot_menu_', path: '/robot/all' },
   {
-    tabname: '에러',
-    icon: '/images/icons/error_menu_',
+    id: 3,
+    name: '로봇',
+    icon: '/images/icons/robot_menu_wh.png',
+    activeIcon: '/images/icons/robot_menu_bk.png',
+    path: '/robot/all',
+  },
+  {
+    id: 4,
+    name: '에러',
+    icon: '/images/icons/error_menu_wh.png',
+    activeIcon: '/images/icons/error_menu_bk.png',
     path: '/error',
   },
-  { tabname: '로그', icon: '/images/icons/log_menu_', path: '/log' },
-  { tabname: '통계', icon: '/images/icons/chart_menu_', path: '/chart' },
+  {
+    id: 5,
+    name: '로그',
+    icon: '/images/icons/log_menu_wh.png',
+    activeIcon: '/images/icons/log_menu_bk.png',
+    path: '/log',
+  },
+  {
+    id: 6,
+    name: '통계',
+    icon: '/images/icons/chart_menu_wh.png',
+    activeIcon: '/images/icons/chart_menu_bk.png',
+    path: '/chart',
+  },
 ];
+
+export default Nav;
